46f5a20df96c16e4183b602c7f1002d24f94d9cb
TonyTheTigrex <anthonysjwilson@protonmail.com>
2024-12-27 13:04:42 +0100

    ROTATE

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         # Enables actually closing the game
         for event in pygame.event.get():
@@ -17,7 +19,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        #limits fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
